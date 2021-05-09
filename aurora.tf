65be20ee8329911fa4c1b47f56335efbbc4c52a5
LM Bibera <lyndon.bibera@headhuntr.io>
2021-05-09 10:55:59 +0800

    initial version with aurora database

---
+++ aurora.tf
@@ -0,0 +1,39 @@
+resource "aws_rds_cluster" "main" {
+  cluster_identifier = "${var.org}-${var.env}-rds-cluster"
+
+  engine_mode    = "serverless"
+  engine         = "aurora-postgresql"
+  engine_version = "10.14"
+
+  enable_http_endpoint = true
+
+  scaling_configuration {
+    auto_pause               = true
+    max_capacity             = 64
+    min_capacity             = 2
+    seconds_until_auto_pause = 1500
+    timeout_action           = "ForceApplyCapacityChange"
+  }
+
+  database_name   = var.rds_dbname
+  master_username = var.rds_username
+  master_password = var.rds_password
+
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  db_subnet_group_name   = aws_db_subnet_group.rds_subnet.name
+  availability_zones     = module.vpc.azs
+
+  deletion_protection     = false
+  skip_final_snapshot     = true
+  backup_retention_period = 7
+  apply_immediately       = true
+
+  tags = local.common_tags
+}
+
+resource "aws_db_subnet_group" "rds_subnet" {
+  name       = "${var.org}-${var.env}-rds-subnet-group"
+  subnet_ids = module.vpc.private_subnets
+
+  tags = local.common_tags
+}
---
+++ variables.tf
@@ -0,0 +1,37 @@
+locals {
+  common_tags = {
+    org     = var.org
+    env     = var.env
+    project = "full-serverless"
+  }
+}
+
+variable "org" {
+  description = "Name of the Organization"
+  type        = string
+  default     = "fs-aws"
+}
+
+variable "env" {
+  description = "Name of the Environment"
+  type        = string
+  default     = "dev"
+}
+
+variable "aws_region" {
+  description = "AWS Resources created region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "rds_dbname" {
+  default = "app_contents"
+}
+
+variable "rds_username" {
+  default = "app_user"
+}
+
+variable "rds_password" {
+  default = "test.123456"
+}
