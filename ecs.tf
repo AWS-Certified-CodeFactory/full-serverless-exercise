caea197301f6f2c77c75d9fcbcea7e67529f3a0c
LM Bibera <lyndon.bibera@headhuntr.io>
2021-05-15 08:27:34 +0800

    launch a simple service

--- ecs.tf
+++ ecs.tf
@@ -9,13 +9,40 @@ resource "aws_ecs_cluster" "main" {
   tags = local.common_tags
 }
 
-resource "aws_ecs_task_definition" "service" {
-  family = "service"
+resource "aws_service_discovery_private_dns_namespace" "internal" {
+  name        = "${var.org}.${var.env}.local"
+  description = "internal dns for ${var.org}.${var.env}"
+  vpc         = module.vpc.vpc_id
+}
+
+resource "aws_service_discovery_service" "cluster_registry" {
+  name = "${var.org}-${var.env}-ecs-cluster-registry"
+
+  dns_config {
+    namespace_id = aws_service_discovery_private_dns_namespace.internal.id
+
+    dns_records {
+      ttl  = 10
+      type = "A"
+    }
+
+    routing_policy = "MULTIVALUE"
+  }
+
+  health_check_custom_config {
+    failure_threshold = 1
+  }
+}
+
+//TODO: https://www.freecodecamp.org/news/how-to-implement-runtime-environment-variables-with-create-react-app-docker-and-nginx-7f9d42a91d70/
+resource "aws_ecs_task_definition" "website" {
+  family = "website"
+
   container_definitions = jsonencode([
     {
-      name      = "first"
-      image     = "service-first"
-      cpu       = 10
+      name      = "website"
+      image     = "nginx"
+      cpu       = 100
       memory    = 512
       essential = true
       portMappings = [
@@ -24,25 +51,41 @@ resource "aws_ecs_task_definition" "service" {
           hostPort      = 80
         }
       ]
-    },
-    {
-      name      = "second"
-      image     = "service-second"
-      cpu       = 10
-      memory    = 256
-      essential = true
-      portMappings = [
-        {
-          containerPort = 443
-          hostPort      = 443
-        }
-      ]
     }
   ])
 
-  volume {
-    name      = "service-storage"
-    host_path = "/ecs/service-storage"
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  requires_compatibilities = ["FARGATE"]
+
+  tags = local.common_tags
+}
+
+resource "aws_ecs_service" "website" {
+  name            = "website"
+  launch_type     = "FARGATE"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.website.arn
+  desired_count   = 4
+
+  network_configuration {
+    subnets          = module.vpc.private_subnets
+    security_groups  = [module.vpc.default_security_group_id]
+    assign_public_ip = false
   }
 
+  //  health_check_grace_period_seconds = 30
+
+  //  load_balancer {
+  //    container_name = ""
+  //    container_port = 0
+  //  }
+
+  service_registries {
+    registry_arn   = aws_service_discovery_service.cluster_registry.arn
+    container_name = "website"
+  }
+
+  tags = local.common_tags
 }
