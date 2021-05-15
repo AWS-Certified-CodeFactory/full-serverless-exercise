6fb836d97e4ce7cafb294ac7ac6493ab4802f909
LM Bibera <lyndon.bibera@headhuntr.io>
2021-05-15 09:24:14 +0800

    expose the service through a load balancer

--- ecs.tf
+++ ecs.tf
@@ -75,12 +75,13 @@ resource "aws_ecs_service" "website" {
     assign_public_ip = false
   }
 
-  //  health_check_grace_period_seconds = 30
+  health_check_grace_period_seconds = 30
 
-  //  load_balancer {
-  //    container_name = ""
-  //    container_port = 0
-  //  }
+  load_balancer {
+    target_group_arn = aws_lb_target_group.cluster_ingress_http.arn
+    container_name   = "website"
+    container_port   = 80
+  }
 
   service_registries {
     registry_arn   = aws_service_discovery_service.cluster_registry.arn
@@ -88,4 +89,66 @@ resource "aws_ecs_service" "website" {
   }
 
   tags = local.common_tags
+}
+
+resource "aws_lb" "cluster_ingress" {
+  name               = "${var.org}-${var.env}-cluster-ingress-alb"
+  load_balancer_type = "application"
+  internal           = false
+
+  security_groups = [aws_security_group.cluster_ingress.id]
+  subnets         = module.vpc.public_subnets
+
+  tags = local.common_tags
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.cluster_ingress.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.cluster_ingress_http.arn
+  }
+}
+
+resource "aws_lb_target_group" "cluster_ingress_http" {
+  name        = "${var.org}-${var.env}-http"
+  target_type = "ip"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = module.vpc.vpc_id
+}
+
+resource "aws_security_group" "cluster_ingress" {
+  name        = "${var.org}-${var.env}-cluster-alb-sg"
+  description = "Allow traffic into the ECS cluster ingress"
+  vpc_id      = module.vpc.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = local.common_tags
+}
+
+resource "aws_security_group_rule" "lb_to_cluster" {
+  description              = "ALB access to the cluster"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  security_group_id        = module.vpc.default_security_group_id
+  source_security_group_id = aws_security_group.cluster_ingress.id
+  type                     = "ingress"
 }
