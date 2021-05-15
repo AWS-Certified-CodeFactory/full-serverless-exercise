808bbef8ee9a18b8603f9030564ea549a9dc4e4d
LM Bibera <lyndon.bibera@headhuntr.io>
2021-05-15 09:45:51 +0800

    generate a simple project

---
+++ shared/apps/api-gateway/src/test/groovy/aws/fs/gateway/ApiGatewaySpec.groovy
@@ -0,0 +1,19 @@
+package aws.fs.gateway
+
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import spock.lang.Specification
+import javax.inject.Inject
+
+@MicronautTest
+class ApiGatewaySpec extends Specification {
+
+    @Inject
+    EmbeddedApplication<?> application
+
+    void 'test it works'() {
+        expect:
+        application.running
+    }
+
+}
