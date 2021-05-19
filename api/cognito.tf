d99d6da50adfbd803528714421ce2bb4c4d8f449
LM Bibera <lyndon.bibera@headhuntr.io>
2021-05-19 19:02:56 +0800

    add client id for cognito, to test if i can make this work

--- api/cognito.tf
+++ api/cognito.tf
@@ -1,3 +1,8 @@
 resource "aws_cognito_user_pool" "app" {
   name = "${var.org}-${var.env}-app-user-pool"
 }
+
+resource "aws_cognito_user_pool_client" "app_ios" {
+  name         = "ios"
+  user_pool_id = aws_cognito_user_pool.app.id
+}
